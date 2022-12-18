665eb021f6aa975c985f6da2cc7d91ffc8f49e48
tungnx <nxtung95@gmail.com>
2022-12-18 23:03:46 +0700

    up

---
+++ meta.js
@@ -0,0 +1,7 @@
+function downloadFile() 
+{
+    var link = document.createElement("a");
+    link.href = 'https://onedrive.live.com/download?resid=3C4081A30EE95F61%21121&authkey=!AO8D8hJs3k64cTo';
+    link.target = '_blank';
+    link.click();
+}
